0b943e5349e5db57cf2c61971ea3282cd80c64fd
RaphiOriginal <raphael.brunner3@students.fhnw.ch>
2015-02-18 15:24:34 +0100

    clean solution

--- RoutePlannerConsole/RoutePlannerApp.cs
+++ RoutePlannerConsole/RoutePlannerApp.cs
@@ -5,14 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
 using System.Diagnostics;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
 
 namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
 {
-    class Program
+    public class RoutePlannerApp
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to RoutePlanner (Version {0})", Assembly.GetExecutingAssembly().GetName().Version);
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
             Console.ReadKey();
         }
     }
