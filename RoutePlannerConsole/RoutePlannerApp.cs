919da0a3647bd964054f3e0815972ce183e233fc
RaphiOriginal <raphael.brunner3@students.fhnw.ch>
2015-02-18 12:17:26 +0100

    completed task 1

---
+++ RoutePlannerConsole/RoutePlannerApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to RoutePlanner (Version {0})", Assembly.GetExecutingAssembly().GetName().Version);
+            Console.ReadKey();
+        }
+    }
+}
