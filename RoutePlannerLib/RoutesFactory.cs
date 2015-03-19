c3da3153eba90d62251e88b338a6993eee83eeb1
RaphiOriginal <raphael.brunner3@students.fhnw.ch>
2015-03-19 13:18:35 +0100

    aufgabe 1

---
+++ RoutePlannerLib/RoutesFactory.cs
@@ -0,0 +1,31 @@
+﻿using RoutePlannerLib.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class RoutesFactory
+    {
+        static public IRoutes Create(Cities cities)
+        {
+            var setting = Settings.Default.RouteAlgorithm;
+            return Create(cities, setting);
+            
+        }
+        static public IRoutes Create(Cities cities, string algorithmClassName)
+        {
+            Assembly assembly = Assembly.LoadFrom(algorithmClassName);
+            /*Type types = assembly.GetType("IRoutes");
+            if(types == null || !types.IsClass){
+                return null;
+            }*/
+            object routeClass = assembly.CreateInstance(algorithmClassName);
+            IRoutes route = routeClass as IRoutes;
+            return route;
+        }
+    }
+}
