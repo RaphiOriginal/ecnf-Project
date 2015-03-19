827c5a806bae8b54882d01bcd1976b66d7135b2f
RaphiOriginal <raphael.brunner3@students.fhnw.ch>
2015-03-19 14:38:13 +0100

    initial serilisation try

---
+++ RoutePlannerLib/SimpleObjectReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
+{
+    public class SimpleObjectReader
+    {
+        StringReader toRead;
+        public SimpleObjectReader(StringReader _toRead)
+        {
+            toRead = _toRead;
+        }
+        public object Next()
+        {
+            return new object();
+        }
+    }
+}
---
+++ RoutePlannerLib/SimpleObjectWriter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
+{
+    public class SimpleObjectWriter
+    {
+        StringWriter stream;
+        public SimpleObjectWriter(StringWriter _stream)
+        {
+            stream = _stream;
+        }
+        public void Next(Object next)
+        {
+            Type type = next.GetType();
+            FieldInfo[] fields = type.GetFields();
+            PropertyInfo[] propertys = type.GetProperties();
+            stream.Write("Instance of "+type.FullName+"\r\n");
+            foreach (FieldInfo f in fields)
+            {
+                if (f.GetValue(next).GetType() == typeof(string))
+                {
+                    stream.Write(f.Name + "=\"" + f.GetValue(next) + "\"\r\n");
+                }
+                else
+                {
+                    stream.Write(f.Name + "=" + f.GetValue(next) + "\r\n");
+                }
+            }
+            foreach (PropertyInfo p in propertys)
+            {
+                stream.Write(p.Name + " is a nested object...\r\n");
+                Next(p);
+            }
+            stream.Write("End of instance\r\n");
+        }
+    }
+}
