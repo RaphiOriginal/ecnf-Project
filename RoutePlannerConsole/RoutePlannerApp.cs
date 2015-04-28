8a79a7c06f41e4668e420fcb45609db17a75317f
RaphiOriginal <raphael.brunner3@students.fhnw.ch>
2015-04-28 13:16:49 +0200

    fixed logging to logfile

--- RoutePlannerConsole/RoutePlannerApp.cs
+++ RoutePlannerConsole/RoutePlannerApp.cs
@@ -19,12 +19,15 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
             Console.WriteLine("Welcome to RoutePlanner (Version {0})", Assembly.GetExecutingAssembly().GetName().Version);
             var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
             Cities bananaRepublik = new Cities();
+            //ohne Exception
             bananaRepublik.ReadCities("citiesTestDataLab2.txt");
             foreach (City city in bananaRepublik.FindNeighbours(wayPoint, 5000))
             {
                 Console.WriteLine(city.Name);
                 Console.WriteLine(city.Location.ToString());
             }
+            //mit Exception
+            bananaRepublik.ReadCities("exception");
             Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
             Console.WriteLine(Directory.GetCurrentDirectory());
             Console.ReadKey();
