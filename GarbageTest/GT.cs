fa8b2ee9c9dd040b83e938d7c01a6fa54545cafa
RaphiOriginal <raphael.brunner3@students.fhnw.ch>
2015-04-28 12:33:49 +0200

    fixed GT

--- GarbageTest/GT.cs
+++ GarbageTest/GT.cs
@@ -5,75 +5,36 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Serialization.Configuration;
+using System.Diagnostics;
 
 namespace Fhnw.Ecnf.RoutePlanner.GarbageTest
 {
     public class GT
     {
-        public static DateTime start;
-        public static DateTime end;
-        public static bool waiting = true;
-        public static bool atTheEnd = false;
+        static Stopwatch time = new Stopwatch();
         
         static void Main(string[] args)
         {
-            GC.RegisterForFullGCNotification(99, 99);
             Console.WriteLine("GC is Server: " + System.Runtime.GCSettings.IsServerGC);
-            var garbageChecker = new Thread(Monitoring);
-            garbageChecker.Start();
-            /*MakeGarbage();
-            start = new DateTime();
-            GC.Collect();
-            Waiting();
-            Conole.WriteLine("Normal: " + (end.Millisecond-start.Millisecond));*/
-            MakeGarbage();
-            start = new DateTime();
-            GC.Collect(0, GCCollectionMode.Default, true);
-            Waiting();
-            Console.WriteLine("Generation 0: " + (end.Millisecond - start.Millisecond));
-            MakeGarbage();
-            start = new DateTime();
-            GC.Collect(1, GCCollectionMode.Default, true);
-            Waiting();
-            Console.WriteLine("Generation 1: " + (end.Millisecond - start.Millisecond));
-            MakeGarbage();
-            start = new DateTime();
-            GC.Collect(2, GCCollectionMode.Default, true);
-            Waiting();
-            Console.WriteLine("Generation 2: " + (end.Millisecond - start.Millisecond));
-            MakeGarbage();
-            start = new DateTime();
-            GC.Collect(0, GCCollectionMode.Optimized, true);
-            Waiting();
-            Console.WriteLine("Optimized Generation 0: " + (end.Millisecond - start.Millisecond));
-            MakeGarbage();
-            start = new DateTime();
-            GC.Collect(1, GCCollectionMode.Optimized, true);
-            Waiting();
-            Console.WriteLine("Optimized Generation 1: " + (end.Millisecond - start.Millisecond));
-            MakeGarbage();
-            start = new DateTime();
-            GC.Collect(2, GCCollectionMode.Optimized, true);
-            Waiting();
-            Console.WriteLine("Optimized Generation 2: " + (end.Millisecond - start.Millisecond));
-            MakeGarbage();
-            start = new DateTime();
-            GC.Collect(0, GCCollectionMode.Forced, true);
-            Waiting();
-            Console.WriteLine("Forced Generation 0: " + (end.Millisecond - start.Millisecond));
-            MakeGarbage();
-            start = new DateTime();
-            GC.Collect(1, GCCollectionMode.Forced, true);
-            Waiting();
-            Console.WriteLine("Forced Generation 1: " + (end.Millisecond - start.Millisecond));
-            MakeGarbage();
-            start = new DateTime();
-            GC.Collect(2, GCCollectionMode.Forced, true);
-            Waiting();
-            atTheEnd = true;
-            Console.WriteLine("Forced Generation 2: " + (end.Millisecond - start.Millisecond));
+            long average = 0;
+            int rounds = 200;
+            for (int i = 0; i < rounds; i++)
+            {
+                time.Start();
+                MakeGarbage();
+                GC.Collect();
+                time.Stop();
+                average += time.ElapsedMilliseconds;
+                Console.WriteLine("Runde "+i+": " + time.ElapsedMilliseconds);
+            }
+            Console.WriteLine("Average: " + (average / rounds));
             Console.ReadKey();
         }
+        /*
+         *Den GC Mode wird im App.config vom GarbageTest geändert false|true
+         *Mit Concurrency dauert es 0 - 28 Millisekunden, im Durchschnitt 15 von 200 Durchläufen sind es 15 Millisekunden.
+         *Ohne Concurrency dauert es 0 - 41 Millisekunden, im Durchschnitt von 200 Durchläufen sind es 24 Millisekunden.
+         */
 
         public static void MakeGarbage()
         {
@@ -82,38 +43,8 @@ namespace Fhnw.Ecnf.RoutePlanner.GarbageTest
                 var j = new DateTimeOffset();
                 var k = "Bananengas!";
                 var l = i;
+                var m = true;
             }
-        }
-        public static void Monitoring()
-        {
-            
-            while (!atTheEnd)
-            {
-                GCNotificationStatus garbageMonitor = GC.WaitForFullGCApproach();
-                if (garbageMonitor == GCNotificationStatus.Succeeded)
-                {
-                    
-                }
-                else if (garbageMonitor == GCNotificationStatus.NotApplicable)
-                {
-                    //Console.WriteLine("GC was not applicable!");
-                }
-                garbageMonitor = GC.WaitForFullGCComplete();
-                if (garbageMonitor == GCNotificationStatus.Succeeded)
-                {
-                    end = new DateTime();
-                    waiting = false;
-                }
-                else if (garbageMonitor == GCNotificationStatus.NotApplicable)
-                {
-
-                }
-            }
-        }
-        public static void Waiting()
-        {
-            while (waiting) { } //Do nothing
-            waiting = true;
         }
     }
 }
